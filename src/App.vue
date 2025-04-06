6df1734ecef84bff7f806999d7841b0b689e2195
Nikita <nikita.vorontsov.1989@mail.ru>
2025-04-06 23:22:38 +0300

    Добавил рандомность каждый раз как происзодит перезагрузка сайта

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,20 @@ import { onMounted, provide, ref } from 'vue';
 const URL_SONG_BIRD = "https://xeno-canto.org/api/2/recordings?query=cnt:brazil";
 const infoBird = ref(null);
 
+// Создаю рандоманое число что бы каждый раз была новая запись изначально
+const randomNumber = ref(0);
+const generateRandom = () => {
+  randomNumber.value = Math.floor(Math.random() * 500); // От 0 до 499
+};
+
+// Принятие API звуков птиц и provide на другие компоненты их
 onMounted(async () => {
   try {
     const response = await fetch(URL_SONG_BIRD);
     const data = await response.json();
     console.log(data);
-    infoBird.value = data.recordings[0]; 
+    generateRandom()
+    infoBird.value = data.recordings[randomNumber]; 
   } catch (error) {
     console.error("Нихуя не работает");
   }
