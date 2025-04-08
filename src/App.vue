f87ce5cad02853d3479fed75b962a9dd5a666925
Nikita <nikita.vorontsov.1989@mail.ru>
2025-04-08 14:13:58 +0300

    Сделал переход на некст уровень, с полной генерацией ответов и вопрсов

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,79 @@ const isLoading = ref(false);
 const error = ref(null);
 const scoreCount = ref(0)
 
+// // Количество вопросов и все переменные для этой фунециии
+// const currentQuestionIndex = ref(0);
+// const countQuestion = 10
+// const quizQuestions = ref([]);
+
+// // Создание заранее 10 вопросов
+
+// async function createArrQuestion() {
+//   try {
+//     isLoading.value = true;
+//     error.value = null;
+    
+//     const response = await fetch(URL_SONG_BIRD);
+//     if (!response.ok) throw new Error("Ошибка API");
+//     const data = await response.json();
+//     const usedIndices = new Set();
+    
+//     // Очищаем массив перед заполнением
+//     quizQuestions.value = [];
+
+//     for (let i = 0; i < countQuestion; i++) {
+//       // Выбираем правильный ответ
+//       const correctIndex = getUniqueRandomIndex(data.recordings.length, usedIndices);
+//       usedIndices.add(correctIndex);
+//       const correctBird = data.recordings[correctIndex];
+      
+//       // Выбираем 5 случайных вариантов
+//       const optionIndices = [];
+//       while (optionIndices.length < 5 && usedIndices.size < data.recordings.length) {
+//         const index = getUniqueRandomIndex(data.recordings.length, usedIndices);
+//         optionIndices.push(index);
+//         usedIndices.add(index);
+//       }
+      
+//       // Формируем вопрос
+//       const allIndices = shuffleArray([correctIndex, ...optionIndices]);
+//       const question = {
+//         correctBird,
+//         options: allIndices.map(index => ({
+//           ...data.recordings[index],
+//           isCorrect: index === correctIndex
+//         }))
+//       };
+      
+//       quizQuestions.value.push(question);
+//     }
+    
+//     // Загружаем первый вопрос
+//     loadQuestion(0);
+    
+//   } catch (err) {
+//     error.value = err.message;
+//     console.error("Ошибка при создании вопросов:", err);
+//   } finally {
+//     isLoading.value = false;
+//   }
+// }
+
+// // Переход к следующему вопросу
+// function nextQuestion() {
+//   loadQuestion(currentQuestionIndex.value + 1);
+// }
+// provide('nextQuestion',nextQuestion)
+// // Загрузка конкретного вопроса
+// function loadQuestion(index) {
+//   if (index >= quizQuestions.value.length) return;
+  
+//   currentQuestionIndex.value = index;
+//   const question = quizQuestions.value[index];
+  
+//   infoBird.value = question.correctBird;
+//   birdArr.value = question.options;
+// }
 // Объявляем provide в setup() ДО onMounted
 provide('infoBird', infoBird);
 provide('birdArr', birdArr);
@@ -67,7 +140,6 @@ async function loadBirdData() {
       ...data.recordings[index],
       isCorrect: index === correctIndex
     }));
-    
   } catch (err) {
     error.value = err.message;
     console.error("Ошибка при загрузке данных:", err);
@@ -84,6 +156,7 @@ const incrementScore = () => {
 };
 provide('incrementScore', incrementScore)
 provide('scoreCount', scoreCount);
+
 // Загрузка данных при монтировании
 onMounted(loadBirdData);
 </script>
@@ -95,7 +168,7 @@ onMounted(loadBirdData);
   <main>
     <CurrentIssue></CurrentIssue>
     <SectionResponseAndDescription></SectionResponseAndDescription>
-    <MyBtn @click=""></MyBtn>
+    <MyBtn @click="loadBirdData"></MyBtn>
   </main>
 </template>
 
--- src/components/MyBtn.vue
+++ src/components/MyBtn.vue
@@ -1,11 +1,30 @@
 <script setup>
+import { ref } from 'vue';
+
+
+const isCooldown = ref(false);
+
+const handleNextLevel = () => {
+  if (isCooldown.value) return;
+  
+  isCooldown.value = true;
+  
+  // Автоматическая разблокировка через 2 секунды
+  setTimeout(() => {
+    isCooldown.value = false;
+  }, 4000);
+};
 
 </script>
 
 <template>
-    <div class='flex items-center'>
-        <button class='p-2 bg-[#3db390] m-5 text-white rounded-xl w-full hover:bg-[#318b70]'>Next Level</button>
-    </div>
+     <button 
+    class="p-2 bg-[#3db390] m-5 text-white rounded-xl w-full hover:bg-[#318b70] disabled:opacity-50"
+    @click="handleNextLevel"
+    :disabled="isCooldown"
+  >
+    Next Level
+  </button>
 </template>
 
 <style> 
